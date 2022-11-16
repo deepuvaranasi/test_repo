595296c944b5f986efd58a65251f3ee3c3cbc7a5
deepuvaranasi <deepthi.varanasi@iths.se>
2022-11-16 02:37:18 +0100

    initial commit

---
+++ runningdogs-data-api/src/main/java/com/example/runningdogs/RunningdogsDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.runningdogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningdogsDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningdogsDataApiApplication.class, args);
+    }
+
+}
